85f511fad7a23cdb590f62596a7107f30f278a87
Colten Bayles <cbayles@outlook.com>
2023-09-21 20:31:59 -0400

    added a animal fact list

--- pizza.py
+++ pizza.py
@@ -8,3 +8,12 @@ for pizzas in pizza_names:
 animals = ['Wolf', 'Mountain Lion', 'Brown Bear', 'Eagle']
 for animal in animals:
     print(animals)
+    print(f"the top preditor animals of the Rocky Mountians are {animals}")
+
+#---------------------------------------
+animal_facts = [
+    ('Wolf', 'Wolves are known to be pact animals who are very social.')
+    ('Mountain Lion', 'Mountain Lions are also called Cougers or Pumas.')
+    ('Brown Bear', 'Brown Bears are brown.')
+    ('Eagle', 'Eagles have excellent vision and can spot pray from extremely far away.')
+]
