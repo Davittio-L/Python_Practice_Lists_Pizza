beea03ab5eaa96e695b3a0db792916ec378ce876
Colten Bayles <cbayles@outlook.com>
2023-09-21 20:22:38 -0400

    added f string to print statement

--- pizza.py
+++ pizza.py
@@ -1,4 +1,4 @@
 #Simple Practice of Lsits:
 pizza_names = ['Pepperoni', 'Hawaiian', 'Cheese']
 for pizzas in pizza_names:
-    print(pizza_names)
+    print(f"Jacks favorite types of pizzas are: {pizza_names}")
