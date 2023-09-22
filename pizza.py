f6ccab4e2390e36f39388d8f4aa2fe63b6fc82b0
Colten Bayles <cbayles@outlook.com>
2023-09-21 20:26:27 -0400

    added new list regarding animals

--- pizza.py
+++ pizza.py
@@ -1,4 +1,10 @@
 #Simple Practice of Lsits:
-pizza_names = ['Pepperoni', 'Hawaiian', 'Cheese']
+pizza_names = ['Pepperoni', 'Hawaiian', 'Cheese',]
 for pizzas in pizza_names:
-    print(f"Jacks favorite types of pizzas are: {pizza_names}")
+    print(pizza_names)
+
+#---------------------------------------------------
+#New List
+animals = ['Wolf', 'Mountain Lion', 'Brown Bear', 'Eagle']
+for animal in animals:
+    print(animals)
